0e5f9398c7962bb7ee691f20e597d68f09ed8f57
yagi <hiro.yagi0245@gmail.com>
2017-01-23 01:44:05 +0900

    add environment

--- eval.go
+++ eval.go
@@ -1,8 +1,31 @@
 package lispy
 
 type Environment struct {
+	Frame     map[string]LObj
+	Enclosing *Environment
 }
 
-func EvalProgram(expr []LObj) {
+func EvalProgram(program []LObj, env Environment) (LObj, Environment) {
+	for i, expr := range program {
+		program[i], env = Eval(expr, env)
+	}
+	return program[len(program)-1], env
+}
 
+func Eval(expr LObj, env Environment) (LObj, Environment) {
+	switch expr.Type {
+	case LispPair:
+		return EvalPair(expr, env)
+	case LispSymbol:
+		return EvalSymbol(expr, env)
+	default:
+		return expr, env
+	}
+}
+
+func EvalPair(expr LObj, env Environment) (LObj, Environment) {
+	return expr, env
+}
+func EvalSymbol(expr LObj, env Environment) (LObj, Environment) {
+	return expr, env
 }

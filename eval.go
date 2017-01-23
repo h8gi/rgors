75c2abb5f9350ea3013ffc401f768d1ead6fdf22
yagi <hiro.yagi0245@gmail.com>
2017-01-23 10:33:44 +0900

    environment lookup

--- eval.go
+++ eval.go
@@ -9,13 +9,29 @@ type Environment struct {
 	Enclosing *Environment
 }
 
+func (env *Environment) LookUp(sym LObj) (LObj, error) {
+	obj, ok := env.Frame[sym.String()]
+	if ok {
+		return obj, nil
+	}
+	nextenv := env.Enclosing
+	if nextenv == nil {
+		return sym, fmt.Errorf("unbound variable: %v", sym)
+	}
+	return nextenv.LookUp(sym)
+}
+
 func EvalProgram(program []LObj) (LObj, error) {
 	var err error
+	var result LObj
 	env := Environment{Frame: map[string]LObj{"a": LObj{Type: LispNumber, Value: 12}}}
-	for i, expr := range program {
-		program[i], err = Eval(expr, env)
+	for _, expr := range program {
+		result, err = Eval(expr, env)
+		if err != nil {
+			break
+		}
 	}
-	return program[len(program)-1], err
+	return result, err
 }
 
 func Eval(expr LObj, env Environment) (LObj, error) {
@@ -27,16 +43,24 @@ func Eval(expr LObj, env Environment) (LObj, error) {
 		return EvalSymbol(expr, env)
 	case LispProcedure:
 		return LObj{}, fmt.Errorf("illegal non-atomic object: %v", expr)
-	default:
+	default: //  self evaluating
 		return expr, err
 	}
 }
 
-func EvalPair(expr LObj, env Environment) (LObj, error) {
+func EvalPair(pair LObj, env Environment) (LObj, error) {
 	var err error
-	return expr, err
+	switch pair.Car.Type {
+	case LispBoolean, LispChar, LispVector, LispNumber, LispString, LispPort, LispNil:
+		return pair.Car, fmt.Errorf("call of non-procedure: %v", pair.Car)
+	case LispProcedure:
+		return Apply(pair.Car, pair.Cdr)
+	default:
+		fun, err := Eval(pair.Car)
+		return Apply(fun, pair.Cdr)
+	}
+	return pair, err
 }
-func EvalSymbol(expr LObj, env Environment) (LObj, error) {
-	var err error
-	return env.Frame[expr.Value.(string)], err
+func EvalSymbol(sym LObj, env Environment) (LObj, error) {
+	return env.LookUp(sym)
 }

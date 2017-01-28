771db94f40b57a7d1f5c3cb7c8584a66b678ce67
yagi <hiro.yagi0245@gmail.com>
2017-01-29 02:48:36 +0900

    rm secd

--- lispObject.go
+++ lispObject.go
@@ -253,3 +253,76 @@ func (obj *LObj) Length() (int, error) {
 func NewVector(objs ...LObj) LObj {
 	return LObj{Type: DTVector, Value: objs}
 }
+
+// env, closure
+
+func NewEnv() *LObj {
+	env := Cons(LispNull, LispNull)
+	return &env
+}
+
+// about environment (list of alist)
+func (env *LObj) LookUp(sym LObj) (LObj, error) {
+	if env.IsNull() {
+		return LispFalse, fmt.Errorf("unbound variable: %v", sym)
+	}
+	currentEnv, err := env.SafeCar()
+	if err != nil {
+		return *env, err
+	}
+	// lookup current environment
+	pair, err := currentEnv.Assq(sym)
+	if err != nil {
+		return currentEnv, err
+	}
+	// found!
+	if pair.ToBool() {
+		return *pair.Cdr, nil
+	}
+	// not found
+	return env.Cdr.LookUp(sym)
+}
+
+// destructive
+func (env *LObj) Define(sym LObj, val LObj) {
+	env.Car.Push(Cons(sym, val))
+}
+
+// return new extended env
+func (parent *LObj) Extend(child *LObj) *LObj {
+	result := Cons(*child, *parent)
+	return &result
+}
+
+func InitialEnv() *LObj {
+	var lispAdd2 = LObj{
+		Type: DTPrimitive,
+		Value: func(obj1, obj2 LObj) (LObj, error) {
+			if obj1.IsNumber() && obj2.IsNumber() {
+				return LObj{Type: Number, Value: obj1.Value.(float64) + obj2.Value.(float64)}, nil
+			} else {
+				return LispFalse, fmt.Errorf("not number's %v + %v", obj1, obj2)
+			}
+		},
+	}
+	sym := NewSymbol("+")
+	env := NewEnv()
+	env.Define(sym, lispAdd2)
+	return env
+}
+
+// closure
+func NewClosure(code LObj, env LObj) LObj {
+	return LObj{
+		Type: DTClosure,
+		Car:  &code,
+		Cdr:  &env,
+	}
+}
+
+func (closure *LObj) Code() *LObj {
+	return closure.Car
+}
+func (closure *LObj) Env() *LObj {
+	return closure.Cdr
+}
